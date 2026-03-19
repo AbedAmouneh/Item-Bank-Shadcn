1f88af3150bd902914126ac4bec546e7e21cc2cc
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-19 15:51:36 +0200

    Add AuthProvider to app root

--- apps/item-bank/src/main.tsx
+++ apps/item-bank/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { getDb } from './db';
+
+import { AuthProvider } from '@item-bank/auth';
 import App from './app/App';
 import '@item-bank/i18n';
 import './styles.css';
-
-getDb();
 
 const queryClient = new QueryClient();
 
@@ -17,7 +16,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
