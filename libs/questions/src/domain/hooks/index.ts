475072b19f00bf2b5682ebdae148de7ed36bcb00
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-21 11:15:47 +0200

    Add useDuplicateQuestion hook and export from domain barrel

--- libs/questions/src/domain/hooks/index.ts
+++ libs/questions/src/domain/hooks/index.ts
@@ -3,5 +3,6 @@ export { useQuestion } from './useQuestion';
 export { useCreateQuestion } from './useCreateQuestion';
 export { useUpdateQuestion } from './useUpdateQuestion';
 export { useDeleteQuestion } from './useDeleteQuestion';
+export { useDuplicateQuestion } from './useDuplicateQuestion';
 export { useSubmitForReview } from './useSubmitForReview';
 export { useImageUpload } from './useImageUpload';
