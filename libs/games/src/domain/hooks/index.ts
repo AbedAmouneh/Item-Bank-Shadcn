9ff3d8bb7cab43a8b4e40e729723fd28df9d52c5
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-23 13:02:06 +0200

    Re-export useFallbackQuestions and usePostGameSession from hooks barrel

--- libs/games/src/domain/hooks/index.ts
+++ libs/games/src/domain/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useGameQuestions } from './UseGameQuestions';
 export { useGameScores } from './UseGameScores';
+export { useFallbackQuestions } from '../UseFallbackQuestions';
+export { usePostGameSession } from '../UsePostGameSession';
